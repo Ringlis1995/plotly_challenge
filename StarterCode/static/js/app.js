19da376f3454246a54b0b82013644e1e511f99f7
Ringlis1995 <roseinglis827@gmail.com>
2020-11-23 18:28:17 -0600

    completed bar chart and bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,11 +8,95 @@ function DrawBubblechart(sampleId)
 function DrawBargraph(sampleId)
 {
     console.log(`DrawBargraph(${sampleId})`);
+    
+    d3.json("samples.json").then((data) => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+        
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+
+        };
+
+        Plotly.newPlot("bar", [barData], barLayout);
+    });
+}
+
+function DrawBubblechart(sampleId)
+{
+    console.log(`DrawBubblechart(${sampleId})`);
+    
+    d3.json("samples.json").then((data) => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        //var yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {size: sample_values, color: otu_ids, colorscale: "Earth"}
+        }
+        
+        var barLayout = {
+            title: "Bacteria Cultures per Sample",
+            margin: {t: 30}
+
+        };
+
+        Plotly.newPlot("bubble", [bubbleData], barLayout);
+    });
 }
 
 function ShowMetadata(sampleId)
 {
     console.log(`ShowMetadata(${sampleId})`);
+
+    d3.json("samples.json").then((data) => {
+
+        var metadata = data.metadata
+        var resultArray = metadata.filter(md => md.id == sampleId);
+        var result = resultArray[0];
+
+        var panel = d3.select(`#sample-metadata`); 
+        panel.html("");
+
+        Object.entries(result).forEach(([key,value]) => {
+
+            var textToShow = `${key} = ${value}`;
+            panel.append("h6").text(textToShow);
+
+        });
+
+    });
+
 }
 
 function optionChanged(newSampleId)
